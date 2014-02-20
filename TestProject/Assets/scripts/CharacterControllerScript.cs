8adcf85b73e06f180e592ac64bc6596f70e6256a
Crystalix <e.gillespie42@hotmail.com>
2014-02-20 13:00:29 +0000

    added crafting system etc

---
+++ TestProject/Assets/scripts/CharacterControllerScript.cs
@@ -0,0 +1,134 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CharacterControllerScript : MonoBehaviour 
+{
+	public enum PlayerState 
+	{
+		Idle,
+		Crafting,
+		SelectingComponents,
+		Serving
+	};
+
+	public PlayerState currentState;
+	//public float maxSpeed = 10.0f;
+	//bool facingRight = true;
+	GameObject CraftingTable;
+	GameObject RecyclingBin;
+	GameObject ComponentsArea;
+	GameObject Till;
+	public TheInventoryScript inventoryScript;
+	public CraftingScript craftingScript;
+	public List<string> itemsTodelete;
+
+	// Use this for initialization
+	void Start () 
+	{
+		CraftingTable = GameObject.Find("CraftingTable");
+		RecyclingBin = GameObject.Find("RecyclingBin");
+		ComponentsArea = GameObject.Find("ComponentsArea");
+		Till = GameObject.Find("Till");
+
+		craftingScript = GetComponent<CraftingScript>();
+		inventoryScript = GetComponent<TheInventoryScript>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		//float moveSide = Input.GetAxis("Horizontal");
+		//float moveUpDown = Input.GetAxis("Vertical");
+		//rigidbody2D.velocity = new Vector2(moveSide*maxSpeed,moveUpDown*maxSpeed);
+	}
+	
+	void Update()
+	{
+		if(Input.GetMouseButtonDown(0))
+		{
+			RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+			if(hit.collider != null)
+				
+			{
+				if(hit.transform.gameObject.tag=="componentsarea")
+				{
+					Debug.Log("component");
+					this.gameObject.transform.position = (ComponentsArea.transform.position + new Vector3(0,-2,0));
+					currentState= PlayerState.SelectingComponents;
+				}
+				else if(hit.transform.gameObject.tag=="recyclingbin")
+				{
+					Debug.Log("recycling");
+					this.gameObject.transform.position = (RecyclingBin.transform.position + new Vector3(0,-1,0));
+				}
+				else if(hit.transform.gameObject.tag=="craftingtable")
+				{
+					Debug.Log("crafting");
+					this.gameObject.transform.position = (CraftingTable.transform.position + new Vector3(2,0,0));
+					this.gameObject.transform.rotation = CraftingTable.transform.rotation;
+					currentState = PlayerState.Crafting;
+				}
+				else if(hit.transform.gameObject.tag=="Till")
+				{
+					this.gameObject.transform.position = (Till.transform.position + new Vector3(0,-1,0));
+					currentState = PlayerState.Serving;
+				}
+				
+			}
+
+		}
+
+		if(currentState == PlayerState.SelectingComponents)
+		{
+
+		}
+	}
+	 public void OnGUI()
+	{
+		if(currentState == PlayerState.SelectingComponents)
+		{
+		foreach (string item in inventoryScript.allComponents)
+		{
+			GUILayout.BeginHorizontal();
+			GUILayout.Box(inventoryScript.componentInventory[item],GUILayout.Width(50.0f), GUILayout.Height(50.0f));
+				if(GUILayout.Button(string.Format("{0})",item)))
+				{
+					Debug.Log(string.Format("Got {0}",item));
+					inventoryScript.playerInventory.Add (item);
+				}
+			GUILayout.EndHorizontal();
+		}
+		}
+
+		if(currentState == PlayerState.Crafting)
+		{
+			foreach(string item in inventoryScript.playerInventory)
+			{
+				GUILayout.BeginHorizontal();
+				if(GUILayout.Button(string.Format("{0}",item)))
+				{
+					craftingScript.craftingItems.Add(item);
+					itemsTodelete.Add(item);
+					//inventoryScript.playerInventory.Remove(item);
+				}
+				GUILayout.EndHorizontal();
+			}
+			foreach(string itemToDelete in itemsTodelete)
+			{
+				inventoryScript.playerInventory.Remove(itemToDelete);
+			}
+
+			if(GUILayout.Button("Craft"))
+			{
+				foreach(string item in craftingScript.craftingItems)
+				{
+					craftingScript.item+=item;
+					craftingScript.Craft();
+				}
+			}
+		}
+	}
+
+}
---
+++ TestProject/Assets/scripts/CraftingScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CraftingScript : MonoBehaviour 
+{
+	public CharacterControllerScript characterControllerScript;
+	public TheInventoryScript inventoryScript;
+	public string item;
+	public Dictionary<string, int> recipeList = new Dictionary<string,int >();
+	public List<string> craftingItems;
+
+	// Use this for initialization
+	void Start () 
+	{
+		characterControllerScript = GetComponent<CharacterControllerScript>();
+		inventoryScript = GetComponent<TheInventoryScript>();
+		recipeList.Add("dogbowl",0);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+
+	public void Craft()
+	{
+		//temporary test code while the inventory and gui interactions arent done yet for component selecting
+		//and inventory
+		//"dog" and "bowl" will later be added by the player selecting the items
+		//item = "dog"+"bowl";
+		if(recipeList.ContainsKey(item))
+		{
+			Debug.Log(item);
+			inventoryScript.playerInventory.Add(item);
+		}
+		characterControllerScript.currentState = CharacterControllerScript.PlayerState.Idle;
+	}
+}
