85eb37274a1cc07ec5da6df465062b36abac87b3
IslayLaphroaig <bdunca201@caledonian.ac.uk>
2014-03-04 14:10:14 +0000

    Restructured AI script. Added states.

--- TestProject/Assets/scripts/CustomerAIScript.cs
+++ TestProject/Assets/scripts/CustomerAIScript.cs
@@ -2,82 +2,112 @@
 using System.Collections;
 using Pathfinding;
 
-public class CustomerAIScript : MonoBehaviour {
-	
-	public Transform target;
-	public Vector3 targetPosition;
-	
-	public  Seeker seeker;
-	private CharacterController controller;
-	
-	//The calculated path
-	public Path path;
-
-	public static GameObject thisCustomer;
-	
-	//The AI's speed per second
-
+public class CustomerAIScript : MonoBehaviour 
+{
+	public Transform target; // The target
+	public Vector3 targetPosition; // The targets position
+	public  Seeker seeker; // The seeker component
+	public float nextWaypointDistance = 0; 	// The max distance from the AI to a waypoint for it to continue to the next waypoint
+	public CustomerNeedsScript theCustomerNeedsScript; // The Customer Needs Script
+	public Path path; // The calculated path
 	[System.NonSerialized]
-	public float speed = 2;
-	
-	//The max distance from the AI to a waypoint for it to continue to the next waypoint
-	public float nextWaypointDistance = 0;
-	
-	//The waypoint the enemies are currently moving towards
-	private int currentWaypoint = 0;
+	public float speed = 2; // The AI's speed per second
+	public static GameObject thisCustomer; // Customer Game Object
+	public enum State{Enter, Idle, Serving, Exit}; // Enum of states
 
-	public CustomerNeedsScript theCustomerNeedsScript;
+	private CharacterController controller; // The character controller
+	private State state; // The current state the customer is in
+	private int currentWaypoint = 0; //The waypoint the enemies are currently moving towards
 
-	public void Start () {
+	IEnumerator Start() 
+	{
 		thisCustomer = this.gameObject;
-	
-		theCustomerNeedsScript = GetComponent<CustomerNeedsScript>();
-		targetPosition = target.transform.position;
-		//Get a reference to the Seeker component that was added earlier
-		seeker = GetComponent<Seeker>();
-		controller = GetComponent<CharacterController>();
-		
-		
-		//Start a new path to the targetPosition, return the result to the OnPathComplete function
-		seeker.StartPath (transform.position,targetPosition, OnPathComplete);
+		state = State.Enter; // Sets the current state to enter
+		while (true) 
+		{
+		switch(state) 
+			{
+				case State.Enter:
+				Enter();
+				break;
+
+				case State.Serving:
+				Serving();
+				break;
+
+				case State.Exit:
+				Exit();
+				break;
+
+				case State.Idle:
+				Idle();
+				break;
+			}
+			yield return 0;
+		}
 	}
-	
-	public void OnPathComplete (Path p) {
-		//Error report
-		if (!p.error) {
+
+	// Enter State
+	private void Enter()
+	{
+		theCustomerNeedsScript = GetComponent<CustomerNeedsScript> (); // Get the customer needs script
+		targetPosition = target.transform.position; // Set the target position
+		seeker = GetComponent<Seeker> (); // Get the seeker component
+		controller = GetComponent<CharacterController> (); // Get the character controller
+		seeker.StartPath (transform.position, targetPosition, OnPathComplete); // Start a new path to the targetPosition, return the result to the OnPathComplete function
+		state = State.Serving; // Change the state to serving state
+	}
+
+	// Serving state
+	private void Serving()
+	{
+	}
+
+	// Exit State
+	private void Exit()
+	{
+	}
+
+	// Idle state
+	private void Idle()
+	{
+	}
+
+	// When the path has been completed...
+	private void OnPathComplete(Path p) 
+	{
+		// Error report
+		if (!p.error) 
+		{
 			path = p;
-			//Reset the waypoint counter
-			currentWaypoint = 1;
+			currentWaypoint = 1; //Reset the waypoint counter
 		}
 	}
 	
-	public void FixedUpdate () {
-		if (path == null) {
-			//No path to move after yet
+	private void FixedUpdate() 
+	{
+		// No path to move after yet
+		if (path == null) 
+		{
 			return;
 		}
-		
-		if (currentWaypoint >= path.vectorPath.Count) {
+
+		// Write to the Debug window when the path has been completed
+		if (currentWaypoint >= path.vectorPath.Count) 
+		{
 			Debug.Log ("End Of Path Reached");
 			return;
 		}
 		
-		//Direction to the next waypoint
-		Vector3 dir = (path.vectorPath[currentWaypoint]-transform.position).normalized;
-		dir *= speed * Time.fixedDeltaTime;
-		controller.Move (dir);
-		
-		//Check if enemies are close enough to the next waypoint
-		//If enemies are, proceed to follow the next waypoint
-		if (Vector3.Distance (transform.position,path.vectorPath[currentWaypoint]) < nextWaypointDistance) {
+		//Check if customers are close enough to the next waypoint, if customers are then proceed to follow the next waypoint
+		if (Vector3.Distance (transform.position,path.vectorPath[currentWaypoint]) < nextWaypointDistance) 
+		{
 			currentWaypoint++;
 			return;
 		}
 
-	
-
-	
+		Vector3 dir = (path.vectorPath[currentWaypoint]-transform.position).normalized; // Direction to the next waypoint
+		dir *= speed * Time.fixedDeltaTime; 
+		controller.Move(dir);
 	}
-
-
 }
