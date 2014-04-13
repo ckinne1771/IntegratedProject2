0b87a2e74ec1b1566bfa308963157b2efa15c039
Crystalix <e.gillespie42@hotmail.com>
2014-04-13 12:01:11 +0100

    made buttons bigger

--- TestProject/Assets/scripts/MainMenu.cs
+++ TestProject/Assets/scripts/MainMenu.cs
@@ -10,14 +10,14 @@ public class MainMenu : MonoBehaviour {
  		
 	
 		GUI.skin = MyGUISkin;
-		GUILayout.BeginArea(new Rect((Screen.width/4-150),(Screen.height/2-20),200,200));
+		GUILayout.BeginArea(new Rect((Screen.width/4-150),Screen.height/2-40,200,200));
 		GUILayout.BeginVertical();
-		if(GUILayout.Button("Level Select"))
+		if(GUILayout.Button("Level Select",GUILayout.Height(Screen.height/10)))
 		{
 			Application.LoadLevel("LevelSelect");
 		}
 
-		if(GUILayout.Button("Quit"))
+		if(GUILayout.Button("Quit",GUILayout.Height(Screen.height/10)))
 		{
 			Application.Quit();
 		}
