7bda67b606f902f047a918eba4c594814f27ef62
Crystalix <e.gillespie42@hotmail.com>
2014-04-13 11:50:26 +0100

    added the new backgrounds

--- TestProject/Assets/scripts/LevelSelect.cs
+++ TestProject/Assets/scripts/LevelSelect.cs
@@ -18,37 +18,29 @@ public class LevelSelect : MonoBehaviour {
 	void OnGUI () 
 	{
 		GUI.skin = MyGUISkin;
-		GUILayout.BeginArea(new Rect(0,Screen.height/2-(Screen.height/4),Screen.width,Screen.height));
-		GUILayout.Box("Level Select");
-		GUILayout.BeginArea(new Rect(0,Screen.height/2-(Screen.height/4),Screen.width,Screen.height));
-		GUILayout.EndArea();
+		GUILayout.BeginArea(new Rect(Screen.width/2-(Screen.width/10),Screen.height/4-50,Screen.width/6,Screen.height));
 		GUILayout.BeginVertical();
-		GUILayout.BeginHorizontal();
-		if(GUILayout.Button("Tutorial",GUILayout.Height(Screen.height/4)))
+		GUILayout.Box("Level Select");
+		if(GUILayout.Button("Tutorial",GUILayout.Height(Screen.height/8)))
 		{
 			Application.LoadLevel("tutorialScene");
 		}
-		if(GUILayout.Button("2",GUILayout.Height(Screen.height/4)))
+		if(GUILayout.Button("2",GUILayout.Height(Screen.height/12)))
 		{
 			Application.LoadLevel("InventoryTest");
 		}
-		GUILayout.EndHorizontal();
-		GUILayout.BeginHorizontal();
-		if(GUILayout.Button("3",GUILayout.Height(Screen.height/4)))
+		if(GUILayout.Button("3",GUILayout.Height(Screen.height/12)))
 		{
 			Application.LoadLevel("Level2");
 		}
-		if(GUILayout.Button("4",GUILayout.Height(Screen.height/4)))
+		if(GUILayout.Button("4",GUILayout.Height(Screen.height/12)))
 		{
 			Application.LoadLevel("Level3");
 		}
-		GUILayout.EndHorizontal();
-		GUILayout.BeginHorizontal();
-		if(GUILayout.Button("Back",GUILayout.Height(Screen.height/4)))
+		if(GUILayout.Button("Back",GUILayout.Height(Screen.height/12)))
 		{
 			Application.LoadLevel("Project");
 		}
-		GUILayout.EndHorizontal();
 		GUILayout.EndVertical();
 		//GUILayout.EndVertical();
 		GUILayout.EndArea();
--- TestProject/Assets/scripts/MainMenu.cs
+++ TestProject/Assets/scripts/MainMenu.cs
@@ -4,21 +4,22 @@ using System.Collections;
 public class MainMenu : MonoBehaviour {
 
 	public GUISkin MyGUISkin;
-
+	public Texture2D thing;
 
 	void OnGUI () {
-
+ 		
+	
 		GUI.skin = MyGUISkin;
-		GUILayout.BeginArea(new Rect((Screen.width/2-150),(Screen.height/2-75),300,150));
+		GUILayout.BeginArea(new Rect((Screen.width/4-150),(Screen.height/2-20),200,200));
 		GUILayout.BeginVertical();
 		if(GUILayout.Button("Level Select"))
 		{
 			Application.LoadLevel("LevelSelect");
 		}
 
-		if(GUILayout.Button("MileStones"))
+		if(GUILayout.Button("Quit"))
 		{
-
+			Application.Quit();
 		}
 		GUILayout.EndVertical();
 		GUILayout.EndArea();
